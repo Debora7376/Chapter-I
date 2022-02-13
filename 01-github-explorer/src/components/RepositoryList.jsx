bae33e441d0da179bb497c11a94a019269e9dfb3
Debora Ferreira <devsouza.root@gmail.com>
2022-02-13 19:16:50 -0300

    Aula 12: Primeiro componente React

---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,20 @@
+const repositoryName = "unform2"
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositorios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositorio
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
